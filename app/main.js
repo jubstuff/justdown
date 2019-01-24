75206e6a8ecf74f5483289bd5475d4ce777cb78f
Giustino Borzacchiello <giustinob@gmail.com>
2019-01-24 21:18:44 +0100

    Read text or md files upon start

--- app/main.js
+++ app/main.js
@@ -1,19 +1,41 @@
-const {app, BrowserWindow} = require('electron');
+const {app, BrowserWindow, dialog} = require( 'electron' );
+
+const fs = require( 'fs' );
 
 let mainWindow = null;
 
-app.on('ready', () => {
-	mainWindow = new BrowserWindow({
+app.on( 'ready', () => {
+	mainWindow = new BrowserWindow( {
 		show: false,
-	});
+	} );
 
-	mainWindow.loadFile('./app/index.html');
+	mainWindow.loadFile( './app/index.html' );
 
-	mainWindow.once('ready-to-show', () => {
+	mainWindow.once( 'ready-to-show', () => {
 		mainWindow.show();
-	});
+		getFileFromUser();
+	} );
 
-	mainWindow.on('closed', () => {
+	mainWindow.on( 'closed', () => {
 		mainWindow = null;
-	})
-});
+	} )
+} );
+
+const getFileFromUser = () => {
+	const files = dialog.showOpenDialog( {
+		properties: ['openFile'],
+		filters: [
+			{name: 'Text Files', extensions: ['txt']},
+			{name: 'Markdown Files', extensions: ['md', 'markdown']}
+		]
+	} );
+
+	if ( ! files ) {
+		return;
+	}
+
+	const file = files[0];
+	const content = fs.readFileSync( file ).toString();
+
+	console.log( content );
+};
