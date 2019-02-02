0eb51418fe46f9e69f64cda17c8bc1ed2f77a23b
Giustino Borzacchiello <giustinob@gmail.com>
2019-02-02 17:33:19 +0100

    Allow to drag md and txt files in the app

--- app/renderer.js
+++ app/renderer.js
@@ -20,6 +20,41 @@ const currentWindow = remote.getCurrentWindow();
 let filePath = null;
 let originalContent = '';
 
+const getDraggedFile = ( event ) => event.dataTransfer.items[0];
+const getDroppedFile = ( event ) => event.dataTransfer.files[0];
+
+const fileTypeIsSupported = ( file ) => {
+	return ['text/plain', 'text/markdown'].includes( file.type );
+};
+
+markdownView.addEventListener( 'dragover', ( event ) => {
+	const file = getDraggedFile( event );
+
+	if ( fileTypeIsSupported( file ) ) {
+		markdownView.classList.add( 'drag-over' );
+	} else {
+		markdownView.classList.add( 'drag-error' );
+	}
+} );
+
+markdownView.addEventListener( 'dragleave', () => {
+	markdownView.classList.remove( 'drag-over' );
+	markdownView.classList.remove( 'drag-error' );
+} );
+
+markdownView.addEventListener( 'drop', ( event ) => {
+	const file = getDroppedFile( event );
+
+	if ( fileTypeIsSupported( file ) ) {
+		mainProcess.openFile( currentWindow, file.path );
+	} else {
+		alert( 'That file type is not supported' );
+	}
+
+	markdownView.classList.remove( 'drag-over' );
+	markdownView.classList.remove( 'drag-error' );
+} );
+
 const renderMarkdownToHtml = ( markdown ) => {
 	htmlView.innerHTML = marked( markdown, {sanitize: true} );
 };
@@ -75,7 +110,12 @@ saveMarkdownButton.addEventListener( 'click', () => {
 	mainProcess.saveMarkdown( currentWindow, filePath, markdownView.value );
 } );
 
-revertButton.addEventListener('click', () => {
+revertButton.addEventListener( 'click', () => {
 	markdownView.value = originalContent;
-	renderMarkdownToHtml(originalContent);
-});
+	renderMarkdownToHtml( originalContent );
+} );
+
+document.addEventListener( 'dragstart', event => event.preventDefault() );
+document.addEventListener( 'dragover', event => event.preventDefault() );
+document.addEventListener( 'dragleave', event => event.preventDefault() );
+document.addEventListener( 'drop', event => event.preventDefault() );
