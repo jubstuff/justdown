e5680e36e8a0280af342bd767e300d017f050006
Giustino Borzacchiello <giustinob@gmail.com>
2019-02-03 17:00:56 +0100

    Add macOS specific behaviors and help menu

--- app/application-menu.js
+++ app/application-menu.js
@@ -16,14 +16,86 @@ const template = [
 	},
 	{
 		label: 'Window',
+		role: 'window',
 		submenu: [
 			{label: 'Minimize', accelerator: 'CommandOrControl+M', role: 'minimize',},
 			{label: 'Close', accelerator: 'CommandOrControl+W', role: 'close',},
 		],
 	},
+	{
+		label: 'Help',
+		role: 'help',
+		submenu: [
+			{
+				label: 'Visit Website',
+				click() { /* To be implemented */ }
+			},
+			{
+				label: 'Toggle Developer Tools',
+				click(item, focusedWindow) {
+					if (focusedWindow) {
+						focusedWindow.webContents.toggleDevTools();
+					}
+				}
+			}
+		],
+	}
 ];
 if ( process.platform === 'darwin' ) {
-	const name = app.getName();
-	template.unshift( {label: name} );
+	const name = 'Fire Sale';
+	template.unshift( {
+		label: name,
+		submenu: [
+			{
+				label: 'About ${name}',
+				role: 'about'
+			},
+			{
+				type: 'separator'
+			},
+			{
+				label: 'Services',
+				role: 'services',
+				submenu: []
+			},
+			{
+				type: 'separator'
+			},
+			{
+				label: 'Hide ${name}',
+				accelerator: 'Command+H',
+				role: 'hide'
+			},
+			{
+				label: 'Hide Others',
+				accelerator: 'Command+Alt+H',
+				role: 'hideothers'
+			},
+			{
+				label: 'Show All',
+				role: 'unhide'
+			},
+			{
+				type: 'separator'
+			},
+			{
+				label: 'Quit ${name}',
+				accelerator: 'Command+Q',
+				click() {
+					app.quit();
+				}
+			},
+		],
+	} );
+
+	const windowMenu = template.find(item => item.label === 'Window');
+	windowMenu.role = 'window';
+	windowMenu.submenu.push(
+		{ type: 'separator' },
+		{
+			label: 'Bring all to front',
+			role: 'front'
+		}
+	);
 }
 module.exports = Menu.buildFromTemplate( template );
